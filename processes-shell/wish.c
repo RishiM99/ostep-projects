1a36d0edd792120a0c0d72d80b2bd29b6729a531
Rishi Maheshwari <rishim@berkeley.edu>
2025-05-11 00:49:17 -0700

    Start shell project

---
+++ processes-shell/wish.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+char *path = "/bin";
+
+void writeError() {
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message)); 
+}
+
+// Counts how many tokens are in a line like "  ls -la  " (2 in that case)
+int countTokens(char *line) {
+    int count = 0;
+    bool inToken = false;
+    for (int i = 0; i < strlen(line); i++) {
+        if (!inToken) {
+            if (strncmp(line + i, " ", 1) != 0 && strncmp(line + i, "\t", 1) != 0) {
+                inToken = true;
+                count++;
+            }
+        }
+        else {
+            if (strncmp(line + i, " ", 1) == 0 || strncmp(line + i, "\t", 1) == 0) {
+                inToken = false;
+            }
+        }
+    }
+
+    return count;
+}
+
+
+char** splitTokens(char *line) {
+    int tokenCount = countTokens(line);
+    char** tokens = (char **) malloc(sizeof(char *) * tokenCount); 
+    int tokenIndex = 0;
+    int i = 0;
+    while (i < strlen(line)) {
+        if (strncmp(line + i, " ", 1) != 0 && strncmp(line + i, "\t", 1) != 0) {
+            int j = i;
+            while (j < strlen(line) && strncmp(line + j, " ", 1) != 0 && strncmp(line + j, "\t", 1) != 0) {
+                j++;
+            }
+            int substringLength = j - i;
+            char *substring = (char *) malloc((substringLength + 1) * sizeof(char));
+            strncpy(substring, line + i, substringLength);
+            substring[substringLength] = '\0';
+            tokens[tokenIndex] = substring;
+            tokenIndex++;
+            i = j;
+        } else {
+            i++;
+        }
+    }
+    return tokens;
+}
+
+void processLine(char *line, size_t len) {
+    char *commandName = strsep(&line, " ");
+    if (strcmp(commandName, "exit") == 0 || strcmp(commandName, "cd") == 0 || strcmp(commandName, "path") == 0) {
+        // Built in command
+    } else {
+        while (line != NULL) {
+
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {  
+    // Interactive mode
+    if (argc == 1) {
+        while (true) {
+            printf("wish> ");
+            char *line = NULL;
+            size_t len = 0;
+
+            if (getline(&line, &len, stdin) == -1) {
+                writeError();
+            }
+
+            processLine(line, len);
+        }
+    }
+}
